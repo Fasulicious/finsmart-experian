bfbd39eea574f1a704b57abe1a2e4567724b78cf
Antonio Soto <fantoniosoto@gmail.com>
2020-07-22 13:41:54 -0500

    writing file

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ const xml = fs.readFileSync('./query.xml');
     body,
     statusCode
   } = response
-  //fs.writeFileSync('response.xml', body.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&quot;/g, '"'))
-  //fs.writeFileSync('response.json', parser.toJson(body))
   const bodyJson = JSON.parse(convert.xml2json(body, { compact: true }))
   const info = bodyJson['soapenv:Envelope']['soapenv:Body']['ns1:consultarResponse']['consultarReturn']['_text']
   const infoJson = JSON.parse(convert.xml2json(info, { compact: true }))
-  console.log(infoJson)
+  //console.log(infoJson.informe)
+  fs.writeFileSync('response.json', JSON.stringify(infoJson.informe))
   console.log(statusCode)
 })()
