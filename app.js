eed3420b72cf72b1f3334669a08ac854bfc9d67d
Antonio Soto <fantoniosoto@gmail.com>
2020-07-22 11:58:20 -0500

    changed to xml-js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 import soapRequest from 'easy-soap-request'
 import fs from 'fs'
-import parser from 'xml2json'
+import convert from 'xml-js'
 
 const url = 'https://demo-servicesesb.experian.com.pe/dhws/services/DHService?wsdl'
 
@@ -23,6 +23,9 @@ const xml = fs.readFileSync('./query.xml');
     statusCode
   } = response
   //fs.writeFileSync('response.xml', body.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&quot;/g, '"'))
-  fs.writeFileSync('response.json', parser.toJson(body))
+  //fs.writeFileSync('response.json', parser.toJson(body))
+  const bodyJson = convert.xml2json(body, { compact: true })
+  const info = bodyJson['soapenv:Envelope']['soapenv:Body']['ns1:consultarResponse']['consultarReturn']['_text']
+  console.log(info)
   console.log(statusCode)
 })()
