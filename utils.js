1f57e0849b4ebfa3085ac1f12c453ab67294be51
Antonio Soto <fantoniosoto@gmail.com>
2020-09-18 13:15:01 -0500

    case 1 logs

--- utils.js
+++ utils.js
@@ -76,11 +76,12 @@ export const getInfo = (data) => {
       const diff = lastReport.getMonth() - currentReport.getMonth()
       switch (diff) {
         case 0:
-          console.log(endeudamiento._attributes.calificacion)
-          console.log(endeudamiento._attributes.fechaReporte)
           cal[diff] += parseInt(endeudamiento._attributes.calificacion, 10)
           break
         case 1:
+          console.log(endeudamiento._attributes.calificacion)
+          console.log(endeudamiento._attributes.fechaReporte)
+          console.log(endeudamiento._attributes.saldo)
           cal[diff] += parseInt(endeudamiento._attributes.calificacion, 10)
           break
         case 2:
