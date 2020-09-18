46ea9ebafbf892aa3e9d41479ae72e6ed472321d
Antonio Soto <fantoniosoto@gmail.com>
2020-09-18 16:11:26 -0500

    remake work

--- t.js
+++
@@ -1,7 +0,0 @@
-const a = require('./test.json')
-const d = a.informe.endeudamientoSBS
-const df = d.filter(e => !e._attributes.codigoPUC.startsWith('84'))
-const puc = df.map(e => e._attributes.codigoPUC)
-const cal = df.map(e => e._attributes.calificacion)
-console.log(new Set(cal))
-console.log(new Set(puc))
